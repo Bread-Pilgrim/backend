9f02b211fd72c193e7f22877feffaa2955fce3d9
chilln <kimjihan77@naver.com>
2025-06-08 22:33:16 +0900

    feat:prometheus 설정파일

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
 
 app = FastAPI(
     title="빵지순례 API",
     description="빵지순례 API",
 )
+
+Instrumentator().instrument(app).expose(app)
