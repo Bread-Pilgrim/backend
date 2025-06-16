fa2612c9ec11ee8225d2fad97764caf6f4ce59bf
chilln <kimjihan77@naver.com>
2025-06-16 17:06:02 +0900

    feat:Users(회원테이블) Base 정의

--- app/model/users.py
+++ app/model/users.py
@@ -1,25 +1,27 @@
-from sqlalchemy import Column, Integer, SmallInteger, String
+from sqlalchemy import Column, DateTime, Integer, SmallInteger, String, func
+from sqlalchemy.orm import declarative_base
 
-from app.model.base_model import BaseModel
+Base = declarative_base()
 
 
-class User(BaseModel):
-    __tablename__: "users"
+class Users(Base):
+    __tablename__ = "users"
 
-    id: int = Column(Integer, primary_key=True, autoincrement=True, comment="회원 ID")
-    login_type: str = Column(
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="회원 ID")
+    login_type = Column(
         String(16),
         nullable=False,
         comment="로그인 타입(KAKAO : 카카오, EMAIL : 이메일)",
     )
-    email: str = Column(String(64), nullable=False, comment="이메일 주소")
-    nickname: str = Column(String(16), nullable=False, comment="닉네임")
-    social_id: str = Column(String(48), nullable=True, comment="소셜로그인 ID")
-    name: str = Column(String(16), nullable=True, comment="이름")
-    age_range: int = Column(SmallInteger, nullable=True, comment="연령대")
-    profile_img: str = Column(
-        String(128),
-        nullable=False,
-        comment="프로필 썸네일 - 카카오톡으로 가져온게 없다면, 닉네임 앞글자 저장",
+    email = Column(String(64), nullable=False, comment="이메일 주소")
+    password = Column(String(64), nullable=True, comment="비밀번호")
+    nickname = Column(String(16), nullable=True, comment="닉네임")
+    social_id = Column(String(64), nullable=True, comment="소셜로그인 ID")
+    name = Column(String(16), nullable=True, comment="이름")
+    age_range = Column(SmallInteger, nullable=True, comment="연령대")
+    profile_img = Column(String(128), nullable=True, comment="프로필 썸네일")
+    gender = Column(String(4), nullable=True)
+    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    updated_at = Column(
+        DateTime, nullable=False, server_default=func.now(), onupdate=func.now()
     )
-    gender: str = Column(String(4), nullable=False)
