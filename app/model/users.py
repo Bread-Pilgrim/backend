fccaacad89fbc7f7aaacf5fb6de8e19b3c99e825
chilln <kimjihan77@naver.com>
2025-06-22 23:37:47 +0900

    feat: 유저-태그 관계 모델(User <-> Tag) 추가

--- app/model/users.py
+++ app/model/users.py
@@ -21,3 +21,18 @@ class Users(Base, DateTimeMixin):
     age_range = Column(SmallInteger, nullable=True, comment="연령대")
     profile_img = Column(String(128), nullable=True, comment="프로필 썸네일")
     gender = Column(String(4), nullable=True)
+
+
+class UserPreference(Base, DateTimeMixin):
+    __tablename__ = "user_preferences"
+
+    user_id = Column(
+        Integer,
+        ForeignKey("users.id", ondelete="CASCADE", comment="users 테이블 id"),
+        primary_key=True,
+    )
+    tag_id = Column(
+        Integer,
+        ForeignKey("tags.id", ondelete="CASCADE", comment="tags 테이블 id"),
+        primary_key=True,
+    )
