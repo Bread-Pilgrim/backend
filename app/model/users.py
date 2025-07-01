570366f20c821cdfb2a022472be012ab423dce47
chilln <kimjihan77@naver.com>
2025-07-02 02:34:17 +0900

    feat:유저테이블 필드 일부수정 / 유저취향 추가

--- app/model/users.py
+++ app/model/users.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, ForeignKey, Integer, SmallInteger, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, SmallInteger, String, Text
 
 from app.model.base import Base
 from app.model.datetime_mixin import DateTimeMixin
@@ -14,16 +14,17 @@ class Users(Base, DateTimeMixin):
         comment="로그인 타입(KAKAO : 카카오, EMAIL : 이메일)",
     )
     email = Column(String(64), nullable=False, comment="이메일 주소")
-    password = Column(String(64), nullable=True, comment="비밀번호")
-    nickname = Column(String(16), nullable=True, comment="닉네임")
+    password = Column(Text(64), nullable=True, comment="비밀번호")
+    nickname = Column(String(24), nullable=True, comment="닉네임")
     social_id = Column(String(64), nullable=True, comment="소셜로그인 ID")
     name = Column(String(16), nullable=True, comment="이름")
     age_range = Column(SmallInteger, nullable=True, comment="연령대")
     profile_img = Column(String(128), nullable=True, comment="프로필 썸네일")
     gender = Column(String(4), nullable=True)
+    is_preferences_set = Column(Boolean, nullable=True)
 
 
-class UserPreference(Base, DateTimeMixin):
+class UserPreferences(Base):
     __tablename__ = "user_preferences"
 
     user_id = Column(
@@ -31,8 +32,18 @@ class UserPreference(Base, DateTimeMixin):
         ForeignKey("users.id", ondelete="CASCADE", comment="users 테이블 id"),
         primary_key=True,
     )
-    tag_id = Column(
+    preference_id = Column(
         Integer,
-        ForeignKey("tags.id", ondelete="CASCADE", comment="tags 테이블 id"),
+        ForeignKey(
+            "preferences.id", ondelete="CASCADE", comment="preferences 테이블 id"
+        ),
         primary_key=True,
     )
+
+
+class Preferences(Base, DateTimeMixin):
+    __tablename__ = "preferences"
+
+    id = Column(Integer, primary_key=True, autoincrement=True, comment="취향항목 ID")
+    name = Column(String(124), comment="취향항목 이름")
+    type = Column(String(40), comment="취향항목 타입")
