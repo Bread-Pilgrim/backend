5cec138a7172610660e999394c23d3983a1cf12a
chilln <kimjihan77@naver.com>
2025-07-02 14:18:40 +0900

    feat:취향항목 조회관련 DTO 정정

---
+++ app/schema/preferences.py
@@ -0,0 +1,19 @@
+from typing import List
+
+from pydantic import BaseModel, Field
+
+
+class PreferenceType(BaseModel):
+    """취향타입 필드 모델."""
+
+    id: int
+    name: str
+
+
+class PreferenceResponseModel(BaseModel):
+    """취향항목 분류모델."""
+
+    atmosphere: List[PreferenceType] = Field(..., description="분위기 항목")
+    bread_type: List[PreferenceType] = Field(..., description="빵종류 항목")
+    flavor: List[PreferenceType] = Field(..., description="맛 항목")
+    c_area: List[PreferenceType] = Field(..., description="상권지역 항목")
