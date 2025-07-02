8136b0e6c44ef40a5d33d20f76d95811913e5dee
chilln <kimjihan77@naver.com>
2025-07-02 15:04:04 +0900

    feat:유저 정보 수정 요청모델

--- app/schema/users.py
+++ app/schema/users.py
@@ -1,10 +1,23 @@
-from typing import List
+from typing import List, Optional
 
 from pydantic import BaseModel, Field
 
 
 class UserPreferencesRequestModel(BaseModel):
+    """유저 취향등록 요청 모델."""
+
     bread_types: List[int] = Field(..., description="빵종류 ID")
     flavors: List[int] = Field(..., description="빵맛")
     atmospheres: List[int] = Field(..., description="빵집 분위기 ID")
     commercial_areas: List[int] = Field(..., description="상권지역 ID")
+
+
+class ModifyUserInfoRequestModel(BaseModel):
+    """유저 정보 수정 요청모델."""
+
+    is_preferences_set: Optional[bool] = Field(None, description="취향설정 완료 여부")
+    age_range: Optional[int] = Field(None, description="연령대")
+    nickname: Optional[str] = Field(None, description="닉네임")
+    profile_img: Optional[str] = Field(None, description="프로필 이미지")
+    gender: Optional[str] = Field(None, description="성별")
+    name: Optional[str] = Field(None, description="이름")
