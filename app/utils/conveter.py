74c37bd9d335e731f1ebcfe7732b7cf9ce9377a0
chilln <kimjihan77@naver.com>
2025-07-04 15:14:05 +0900

    feat:투어 API 원하는 response형태로 변환해서 반환하는 메소드

--- app/utils/conveter.py
+++ app/utils/conveter.py
@@ -6,3 +6,17 @@ def user_info_to_id(user_info) -> int:
 
     user_id = user_info.data.get("user_id")
     return int(user_id)
+
+
+def transform_tour_response(response: dict):
+    """투어 API 원하는 response형태로 변환해서 반환하는 메소드."""
+
+    res = response.get("response")
+    try:
+        if res:
+            items = res.get("body").get("items")
+            if items != "":
+                item = items.get("item")
+                return item
+    except Exception as e:
+        raise UnknownExceptionError(str(e))
