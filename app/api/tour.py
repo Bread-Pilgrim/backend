dcd861e28deff8066e5210fe56080bd943424ba7
chilln <kimjihan77@naver.com>
2025-07-09 18:16:39 +0900

    docs:지역코드 description 추가

--- app/api/tour.py
+++ app/api/tour.py
@@ -22,12 +22,26 @@ router = APIRouter(
     response_description="""
     1. 500 에러 예시 : DB 이슈
     """,
+    description="""해당 지역의 행사정보 조회 API\n 
+    1 :	서면•전포
+    2 :	광안리•민락
+    3 :	남포•광복
+    4 :	연산•거제
+    5 :	해운대•송정
+    6 :	동래•온천장
+    7 :	하단•다대포
+    8 :	괘법•엄궁
+    9 :	장전•부산대
+    10 : 기장읍•일광
+    11 : 명지•대저
+    12 : 대연•문현
+    13 : 기타 지역
+    14 : 전체
+    """,
 )
 async def show_region_event_popup(
     region_code: str, db=Depends(get_db), user_info=Depends(verify_token)
 ):
-    """해당 지역의 행사정보 조회 API"""
-
     return BaseResponse(
         data=await TourService(db=db).get_region_event(int(region_code))
     )
