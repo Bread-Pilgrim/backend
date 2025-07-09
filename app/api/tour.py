1386cf1d7dc2738178866b2afd3a2529c7a56afd
chilln <kimjihan77@naver.com>
2025-07-09 19:29:28 +0900

    fix:주변 관광지 추천하는 API 여행 카테고리 파라미터 추가, 중첩 List 가공처리 로직 삭제

--- app/api/tour.py
+++ app/api/tour.py
@@ -48,13 +48,41 @@ async def show_region_event_popup(
 
 
 @router.get(
-    "/region/{region_code}",
+    "/region",
     response_model=BaseResponse[List[TourResponseModel]],
     responses=ERROR_UNKNOWN,
     response_description="""
     1. 500 에러 예시 : DB 이슈
     """,
+    description="""주변 관광지 추천하는 API\n 
+    [지역코드]
+    1 :	서면•전포
+    2 :	광안리•민락
+    3 :	남포•광복
+    4 :	연산•거제
+    5 :	해운대•송정
+    6 :	동래•온천장
+    7 :	하단•다대포
+    8 :	괘법•엄궁
+    9 :	장전•부산대
+    10 : 기장읍•일광
+    11 : 명지•대저
+    12 : 대연•문현
+    13 : 기타 지역
+    14 : 전체\n
+    
+    [관광지 카테고리 코드]
+    A01 : 자연,
+    A02 : 인문(문화/예술/역사),
+    A03 : 레포츠,
+    A04 : 쇼핑,
+    C01 : 추천코스,
+    """,
 )
-async def get(region_code: str, db=Depends(get_db)):
-    """주변 관광지 추천하는 API"""
-    return BaseResponse(data=await TourService(db=db).get_region_tour(int(region_code)))
+async def get(
+    region_code: str, tour_cat: str, db=Depends(get_db), user_info=Depends(verify_token)
+):
+
+    return BaseResponse(
+        data=await TourService(db=db).get_region_tour(int(region_code), tour_cat)
+    )
