cb164ab1e350bae34fbbc5e7f4ffb3ecf78d54cb
chilln <kimjihan77@naver.com>
2025-07-11 14:46:05 +0900

    feat:지역 추천 빵집 조회 API

--- app/repositories/bakery_repo.py
+++ app/repositories/bakery_repo.py
@@ -1,19 +1,15 @@
-from sqlalchemy import and_, distinct, select
+from sqlalchemy import and_, literal_column, select
 from sqlalchemy.orm.session import Session
 
 from app.core.exception import UnknownExceptionError
-from app.model.area import CommercialAreas
 from app.model.bakery import Bakery, BakeryPreference, BakeryThumbnail, OperatingHour
 from app.model.users import UserPreferences
-from app.schema.bakery import PersonalRecommendBakery
-from app.schema.common import AreaCodeModel
+from app.schema.bakery import RecommendBakery
 
 
 class BakeryRepository:
     def __init__(self, db: Session) -> None:
         self.db = db
-
-    from sqlalchemy import true
 
     async def get_bakeries_by_personal(
         self, area_codes: list[str], user_id: int, target_day_of_week: int
@@ -28,6 +24,7 @@ class BakeryRepository:
 
             stmt = (
                 select(
+                    Bakery.id,
                     Bakery.name,
                     Bakery.avg_rating,
                     Bakery.review_count,
@@ -61,7 +58,52 @@ class BakeryRepository:
 
             res = self.db.execute(stmt).mappings().all()
             return [
-                PersonalRecommendBakery(
+                RecommendBakery(
+                    bakery_id=r.id,
+                    name=r.name,
+                    avg_rating=r.avg_rating,
+                    review_count=r.review_count,
+                    is_opened=r.is_opened,
+                    img_url=r.img_url,
+                )
+                for r in res
+            ]
+        except Exception as e:
+            raise UnknownExceptionError(str(e))
+
+    async def get_bakery_by_area(self, area_codes: list[str], target_day_of_week: int):
+        """지역코드로 베이터리 조회하는 쿼리."""
+
+        try:
+
+            stmt = (
+                select(
+                    literal_column("DIXTINCT ON (b.id) b.id"),
+                    Bakery.id.label("bakery_id"),
+                    Bakery.name,
+                    Bakery.avg_rating,
+                    Bakery.review_count,
+                    OperatingHour.is_opened,
+                    BakeryThumbnail.img_url,
+                )
+                .select_from(Bakery.alias("b"))
+                .join(OperatingHour, OperatingHour.bakery_id == literal_column("b.id"))
+                .join(
+                    BakeryThumbnail, BakeryThumbnail.bakery_id == literal_column("b.id")
+                )
+                .where(
+                    OperatingHour.day_of_week == target_day_of_week,
+                    Bakery.commercial_area_id.in_(area_codes),
+                )
+                .order_by(Bakery.avg_rating.desc)
+                .limit(10)
+            )
+
+            res = self.db.execute(stmt).mappings().all()
+
+            return [
+                RecommendBakery(
+                    bakery_id=r.id,
                     name=r.name,
                     avg_rating=r.avg_rating,
                     review_count=r.review_count,
--- app/services/bakery_service.py
+++ app/services/bakery_service.py
@@ -12,6 +12,8 @@ class BakeryService:
         self.db = db
 
     async def get_bakeries_by_personal(self, area_code: str, user_info):
+        """유저의 취향이 반영된 빵집 조회하는 비즈니스 로직."""
+
         user_id = user_info_to_id(user_info)
 
         # 구분자로 받은 지역코드 list로 반환
@@ -24,4 +26,14 @@ class BakeryService:
             area_codes=area_codes,
             user_id=user_id,
             target_day_of_week=target_day_of_week,
+        )
+
+    async def get_bakery_by_area(self, area_code: str):
+        """지역코드 기반으로 빵집 조회하는 비즈니스 로직."""
+
+        area_codes = parse_area_codes(area_code)
+        target_day_of_week = datetime.today().weekday() + 1
+
+        return await BakeryRepository(self.db).get_bakery_by_area(
+            area_codes, target_day_of_week
         )
