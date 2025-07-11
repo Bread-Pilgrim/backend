fcec91ffd228e7ecfb3d78c9e92bc701908c4d26
chilln <kimjihan77@naver.com>
2025-07-11 14:44:13 +0900

    feat:천 빵집 모델  필드 추가

--- app/schema/bakery.py
+++ app/schema/bakery.py
@@ -1,9 +1,10 @@
 from pydantic import BaseModel, Field
 
 
-class PersonalRecommendBakery(BaseModel):
-    """취향 빵집 모델"""
+class RecommendBakery(BaseModel):
+    """추천 빵집 모델"""
 
+    bakery_id: int = Field(..., description="베이커리 id")
     name: str = Field(..., description="베이커리 상호명")
     avg_rating: float = Field(..., description="평균별점")
     review_count: float = Field(..., description="리뷰 개수")
