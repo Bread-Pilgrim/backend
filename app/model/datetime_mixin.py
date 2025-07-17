b97399a4c77c801df61a45b83039e2e4b7c10f7f
chilln <kimjihan77@naver.com>
2025-07-17 19:26:14 +0900

    refactor:타임존 utc 기준으로 통일

--- app/model/datetime_mixin.py
+++ app/model/datetime_mixin.py
@@ -2,7 +2,12 @@ from sqlalchemy import Column, DateTime, func
 
 
 class DateTimeMixin:
-    created_at = Column(DateTime, nullable=False, server_default=func.now())
+    created_at = Column(
+        DateTime(timezone=True), nullable=False, server_default=func.now()
+    )
     updated_at = Column(
-        DateTime, nullable=False, server_default=func.now(), onupdate=func.now()
+        DateTime(timezone=True),
+        nullable=False,
+        server_default=func.now(),
+        onupdate=func.now(),
     )
