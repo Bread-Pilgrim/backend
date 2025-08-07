4f3d451d22bd3af9027e3ca859e1247c3062cd8e
chilln <kimjihan77@naver.com>
2025-08-07 19:47:30 +0900

    refactor:로그인 API - 세션 커밋 로직을 외부에서 처리하도록 리포지토리 및 서비스 로직 리팩토링

--- app/repositories/auth_repo.py
+++ app/repositories/auth_repo.py
@@ -1,4 +1,3 @@
-from app.core.exception import UnknownException
 from app.model.users import Users
 
 
@@ -6,7 +5,9 @@ class AuthRepository:
     def __init__(self, db) -> None:
         self.db = db
 
-    async def get_user_id_by_socials(self, login_type: str, email: str, social_id: str):
+    async def get_user_id_by_socials(
+        self, login_type: str, email: str, social_id: str
+    ) -> int | None:
         """소셜로그인 유저 정보 기반으로 유저 id값 조회하는 쿼리."""
 
         user = (
@@ -24,28 +25,18 @@ class AuthRepository:
     async def sign_up_user(self, login_type: str, data):
         """회원가입 메소드."""
 
-        try:
-            add_data = {key: value for key, value in data if value is not None}
-            user = Users(**{**add_data, "login_type": login_type})
-            self.db.add(user)
-            self.db.commit()
-            self.db.refresh(user)
-            return user.id
-        except Exception as e:
-            self.db.rollback()
-            raise UnknownException(detail=str(e))
+        add_data = {key: value for key, value in data if value is not None}
+        user = Users(**{**add_data, "login_type": login_type})
+        self.db.add(user)
+        self.db.flush()
+        self.db.refresh(user)
+        return user.id
 
     async def check_completed_onboarding(self, user_id: int) -> bool:
-        """온보딩 완료사항여부 반환하는 메소드."""
+        """온보딩 완료사항여부 반환하는 쿼리."""
 
-        try:
-            res = (
-                self.db.query(Users.is_preferences_set)
-                .filter(Users.id == user_id)
-                .first()
-            )
+        res = (
+            self.db.query(Users.is_preferences_set).filter(Users.id == user_id).first()
+        )
 
-            return res.is_preferences_set
-
-        except Exception as e:
-            raise UnknownException(detail=str(e))
+        return res.is_preferences_set if res else False
